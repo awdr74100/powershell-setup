084428712e0fa03d75d48f5d5bfbaad744b521aa
藍奕濡 <a78945612385238@gmail.com>
2021-04-02 15:47:02 +0800

    init

---
+++ Microsoft.Powershell_profile.ps1
@@ -0,0 +1,41 @@
+Import-Module posh-git
+
+function touch {set-content -Path ($args[0]) -Value ($null)}
+
+[ScriptBlock]$Prompt = {
+    $lastCommandSuccess = $?
+    $realLASTEXITCODE = $global:LASTEXITCODE
+    $errorCode = 0
+    if ($lastCommandSuccess -eq $false) {
+        #native app exit code
+        if ($realLASTEXITCODE -is [int]) {
+            $errorCode = $realLASTEXITCODE
+        }
+        else {
+            $errorCode = 1
+        }
+    }
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "C:\Users\x06u4\scoop\apps\oh-my-posh\3.130.1\bin\oh-my-posh.exe"
+    $cleanPWD = $PWD.ProviderPath.TrimEnd("\")
+    $startInfo.Arguments = "-config=""C:\Users\x06u4\scoop\apps\oh-my-posh\current\themes\custom.omp.json"" -error=$errorCode -pwd=""$cleanPWD"""
+    $startInfo.Environment["TERM"] = "xterm-256color"
+    $startInfo.CreateNoWindow = $true
+    $startInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    if ($PWD.Provider.Name -eq 'FileSystem') {
+      $startInfo.WorkingDirectory = $PWD.ProviderPath
+    }
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start() | Out-Null
+    $standardOut = $process.StandardOutput.ReadToEnd()
+    $process.WaitForExit()
+    $standardOut
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    #remove temp variables
+    Remove-Variable realLASTEXITCODE -Confirm:$false
+    Remove-Variable lastCommandSuccess -Confirm:$false
+}
+Set-Item -Path Function:prompt -Value $Prompt -Force
---
+++ README.md
@@ -0,0 +1,22 @@
+# PowerShell Core Setup
+
+About [PowerShell Core](https://github.com/PowerShell/PowerShell) any setting
+
+## Requirements
+
+- [Windows Terminal](https://github.com/microsoft/terminal)
+- [Scoop](https://github.com/lukesampson/scoop)
+
+## Installation package
+
+- [posh-git](https://github.com/dahlbyk/posh-git)
+
+```shell
+$ scoop install posh-git
+```
+
+- [oh-my-posh](https://github.com/JanDeDobbeleer/oh-my-posh)
+
+```shell
+$ scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+```
